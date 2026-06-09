762a98cf3b2e35e3c222abb51978aec77504c3d4
Llaitureo <fe.llaitureo@duocuc.cl>
2026-06-09 16:57:56 -0400

    Fix | Arreglo de agregación de subnets en main.tf

--- infra/terraform/kubeForm/main.tf
+++ infra/terraform/kubeForm/main.tf
@@ -7,35 +7,42 @@ terraform {
   }
 }
 
-# EKS
+# === #
+# EKS #
+# === #
 
 resource "aws_eks_cluster" "eks" {
-    name = var.name_cluster
+    name     = var.name_cluster
     role_arn = data.aws_iam_role.labrole.arn
     vpc_config {
-      subnet_ids = [aws_subnet.public[0].id,
-                    aws_subnet.public[1].id]
+      subnet_ids = [
+        aws_subnet.public[0].id,
+        aws_subnet.public[1].id,
+        aws_subnet.private[0].id,
+        aws_subnet.private[1].id
+      ]
     }
 }
 
 resource "aws_eks_node_group" "workers" {
-    cluster_name = aws_eks_cluster.eks.name
+    cluster_name    = aws_eks_cluster.eks.name
     node_group_name = var.name_node
-    node_role_arn = data.aws_iam_role.labrole.arn
-    subnet_ids = [aws_subnet.private[0].id,
-                    aws_subnet.private[1].id,
-                    aws_subnet.public[0].id,
-                    aws_subnet.public[1].id]
+    node_role_arn   = data.aws_iam_role.labrole.arn
+    
+    subnet_ids = [
+      aws_subnet.private[0].id,
+      aws_subnet.private[1].id
+    ]
+
     scaling_config {
       desired_size = 2
-      max_size = 2
-      min_size = 1
+      max_size     = 2
+      min_size     = 1
     }
 
     instance_types = ["t3.medium"]
-    capacity_type = "ON_DEMAND"
+    capacity_type  = "ON_DEMAND"
 }
-
 # ECR
 
 resource "aws_ecr_repository" "backend_Ventas_repo" {

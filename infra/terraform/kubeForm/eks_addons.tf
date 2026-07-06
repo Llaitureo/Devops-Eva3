e394aa0e4e35fddc11e26bdacd51410cbe3ed36a
Llaitureo <fe.llaitureo@duocuc.cl>
2026-07-06 14:54:52 -0400

    Hotfix | capa 8.

--- infra/terraform/kubeForm/eks_addons.tf
+++ infra/terraform/kubeForm/eks_addons.tf
@@ -1,6 +1,11 @@
 resource "aws_eks_addon" "cloudwatch_observability" {
-  cluster_name             = "innovatech-cluster"
+  cluster_name             = aws_eks_cluster.eks.name
   addon_name               = "amazon-cloudwatch-observability"
 
   service_account_role_arn = data.aws_iam_role.labrole.arn
+
+  depends_on = [
+    aws_eks_cluster.eks,
+    aws_eks_node_group.workers
+  ]
 }
